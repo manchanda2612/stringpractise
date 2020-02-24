fe25529fbdc6525952e410d02fb14165945f1004
Neeraj Manchanda <manchanda2612@gmail.com>
2020-02-24 11:46:32 +0530

    udemy code integrated and stack array implementation is done

---
+++ app/src/main/java/sample/com/practise/stack_array/StackArrayImplementation.java
@@ -0,0 +1,68 @@
+package sample.com.practise.stack_array;
+
+public class StackArrayImplementation {
+
+    int arr[];
+    int top = -1;
+
+    public void createStack(int size) {
+        arr = new int[size];
+        System.out.println("Stack is created of lenght " + size);
+    }
+
+
+    public void pushValueOntoStack(int value) {
+
+        if(checkWhetherStackIsFull()) {
+            System.out.println("Stack is Full");
+        } else {
+            top++;
+            arr[top] = value;
+            System.out.println("Pushed Value" + arr[top]);
+        }
+    }
+
+    public void popValueFromStack() {
+        if(checkWhetherStackIsEmpty()) {
+            System.out.println("Stack is Empty");
+        } else {
+            System.out.println("Popped Value" + arr[top]);
+            top--;
+        }
+    }
+
+    public void peekValueFromStack() {
+        if(checkWhetherStackIsEmpty()) {
+            System.out.println("Stack is Empty");
+        } else {
+            System.out.print("Peek Value is " + arr[top]);
+        }
+    }
+
+    private boolean checkWhetherStackIsEmpty() {
+        return top == -1;
+    }
+
+
+    private boolean checkWhetherStackIsFull(){
+        return top == arr.length - 1;
+    }
+
+
+    public void deletionOfStack() {
+        arr = null;
+    }
+
+    public void traverseStack() {
+        if(top > -1 && null != arr) {
+            for (int i = top; i >= 0; i--) {
+                System.out.print(arr[i] + "\t");
+            }
+            System.out.print("\n");
+        } else {
+            System.out.print("Stack is Empty");
+        }
+
+    }
+
+}
---
+++ app/src/main/java/sample/com/practise/stack_array/StackArrayMainClass.java
@@ -0,0 +1,40 @@
+package sample.com.practise.stack_array;
+
+public class StackArrayMainClass {
+
+    public static void main(String[] args) {
+
+        StackArrayImplementation stackArrayImplementation = new StackArrayImplementation();
+
+        stackArrayImplementation.createStack(10);
+        stackArrayImplementation.pushValueOntoStack(10);
+        stackArrayImplementation.pushValueOntoStack(20);
+        stackArrayImplementation.pushValueOntoStack(30);
+        stackArrayImplementation.pushValueOntoStack(40);
+        stackArrayImplementation.pushValueOntoStack(50);
+        stackArrayImplementation.pushValueOntoStack(60);
+        stackArrayImplementation.pushValueOntoStack(70);
+        stackArrayImplementation.pushValueOntoStack(80);
+        stackArrayImplementation.pushValueOntoStack(90);
+        stackArrayImplementation.pushValueOntoStack(100);
+        stackArrayImplementation.pushValueOntoStack(110);
+
+        stackArrayImplementation.traverseStack();
+
+
+        stackArrayImplementation.popValueFromStack();
+        stackArrayImplementation.popValueFromStack();
+        stackArrayImplementation.popValueFromStack();
+        stackArrayImplementation.popValueFromStack();
+
+        stackArrayImplementation.traverseStack();
+
+
+        stackArrayImplementation.deletionOfStack();
+        stackArrayImplementation.traverseStack();
+
+
+
+
+    }
+}
